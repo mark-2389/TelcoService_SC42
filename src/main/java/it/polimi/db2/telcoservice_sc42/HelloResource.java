9f7b894047ea61538fba9819014540b36ef60d06
Marco D'Amico <marco.damico@mail.polimi.it>
2021-12-16 18:45:52 +0100

    Initial commit

---
+++ src/main/java/it/polimi/db2/telcoservice_sc42/HelloResource.java
@@ -0,0 +1,14 @@
+package it.polimi.db2.telcoservice_sc42;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/it/polimi/db2/telcoservice_sc42/HelloServlet.java
@@ -0,0 +1,36 @@
+package it.polimi.db2.telcoservice_sc42;
+
+import java.io.*;
+import java.sql.DriverManager;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String DB_URL = "jdbc:mysql://localhost:3306/telcoservice_db"; //Replace with your own configuration
+        final String USER = "Mark"; //Replace with your own configuration
+        final String PASS = "6428M@rco"; //Replace with your own configuration
+        String result = "Connection worked";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (Exception e) {
+            result = "Connection failed"; e.printStackTrace();
+        }
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println(result);
+        out.close();
+    }
+
+    public void destroy() {
+    }
+}
