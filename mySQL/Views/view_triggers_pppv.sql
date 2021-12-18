925b102067200559a22f6b01f96d146a67c4f80a
Marco D'Amico <marco.damico@mail.polimi.it>
2021-12-18 12:39:55 +0100

    Add table and trigger for View_triggers_PurchasePerPackageValidity.

---
+++ mySQL/Views/view_triggers_pppv.sql
@@ -0,0 +1,31 @@
+create table purchase_per_package_validity (
+	PACKAGEID int,
+    VALIDITYID int,
+    PURCHASES int
+);
+
+delimiter //
+
+create trigger new_package_validity
+after insert on validity
+for each row
+begin
+	insert into purcahse_per_package_validity value (new.ID, new.PACKAGEID, 0);
+end; //
+
+delimiter ;
+
+delimiter // 
+
+create trigger new_purchase_validity
+after update on telcoservice_db.order
+for each row
+begin
+	if ( old.isvalid != ACCEPTED and new.isvalid = ACCEPTED) then
+		update purchase_per_package_valdity PPPV
+			set PPPV.purchases = PPPV.purchases + 1
+            where PPPV.PACKAGEID = new.PACKAGEID and PPPV.VALIDITYID = new.validityid;
+    end if;
+end; // 
+
+delimiter ;
