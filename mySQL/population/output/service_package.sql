c3647344d7eada063e4dddf11ada21ae1f7e4fdd
Marco D'Amico <marco.damico@mail.polimi.it>
2021-12-20 00:16:57 +0100

    Add population for service_package and optional_product.

---
+++ mySQL/population/output/service_package.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS `telcoservice_db`;
+USE `telcoservice_db`;
+INSERT INTO `service_package`
+(name) VALUES 
+('servicePackage_1'), 
+('servicePackage_2'), 
+('servicePackage_3'), 
+('servicePackage_4'), 
+('servicePackage_5'), 
+('servicePackage_6'), 
+('servicePackage_7'), 
+('servicePackage_8'), 
+('servicePackage_9'), 
+('servicePackage_10');
