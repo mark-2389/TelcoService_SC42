a94af1c616be5162ee4ea992870a0bc46e5a73b7
Niccolò Didoni <niccolo.didoni@gmail.com>
2021-12-19 14:19:41 +0100

    Removed merge references from sql definition files.

--- mySQL/Tables/telcoservice_db_routines.sql
+++ mySQL/Tables/telcoservice_db_routines.sql
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 CREATE DATABASE  IF NOT EXISTS `telcoservice_db`;
 USE `telcoservice_db`;
-=======
-CREATE DATABASE  IF NOT EXISTS `telcoservice_db`;
-USE `telcoservice_db`;
->>>>>>> branch 'main' of https://github.com/niccolodidoni/TelcoService_SC42
+
