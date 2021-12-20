fc2633254635496ea00f6dc9f35df748d6ce6c0c
Marco D'Amico <marco.damico@mail.polimi.it>
2021-12-20 17:34:13 +0100

    Updated HomePageServlet.

--- src/main/java/it/polimi/db2/telcoservice_sc42/servlets/GoToHomePageServlet.java
+++ src/main/java/it/polimi/db2/telcoservice_sc42/servlets/GoToHomePageServlet.java
@@ -11,11 +11,14 @@ public class GoToHomePageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // If the user is not logged in (not present in session) redirect to the login
-        String loginPath = getServletContext().getContextPath() + "/WEB-INF/home.jsp";
+        String loginPath = getServletContext().getContextPath() + "/home.jsp";
         HttpSession session = request.getSession();
-        //if (session.isNew() || session.getAttribute("user") == null) {
+
+        if (session.isNew() || session.getAttribute("user") == null) {
             response.sendRedirect(loginPath);
-        //    return;
+        }
+
+        return;
     }
 
     @Override
