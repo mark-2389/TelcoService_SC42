ea9a0818b6494681da962e203ff2ea54be948332
Niccolò Didoni <niccolo.didoni@gmail.com>
2021-12-28 19:28:30 +0100

    Add file to generate internet service tables.

--- mySQL/Tables/telcoservice_db_internet_service.sql
+++ mySQL/Tables/telcoservice_db_internet_service.sql
@@ -2,16 +2,29 @@ CREATE DATABASE  IF NOT EXISTS `telcoservice_db`;
 USE `telcoservice_db`;
 
 DROP TABLE IF EXISTS `internet_service`;
+DROP TABLE IF EXISTS `mobile_internet_service`;
+DROP TABLE IF EXISTS `fixed_internet_service`;
 
-CREATE TABLE `internet_service` (
+CREATE TABLE `mobile_internet_service` (
 
   `ID` int PRIMARY KEY,
-  `IS_MOBILE` bit(1),
   `GB` int,
   `GB_FEE` decimal(6,2),
 
-  CONSTRAINT `internet_service_ibfk_1` FOREIGN KEY (`ID`)
+  CONSTRAINT `mobile_internet_service_ibfk_1` FOREIGN KEY (`ID`)
    REFERENCES `service` (`ID`)
       ON DELETE CASCADE
       ON UPDATE CASCADE
+);
+
+CREATE TABLE `fixed_internet_service` (
+
+    `ID` int PRIMARY KEY,
+    `GB` int,
+    `GB_FEE` decimal(6,2),
+
+    CONSTRAINT `fixed_internet_service_ibfk_1` FOREIGN KEY (`ID`)
+        REFERENCES `service` (`ID`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
