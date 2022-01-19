111444f72a1a59550180d876b0b40482bf4c8bf0
Niccolò Didoni <niccolo.didoni@gmail.com>
2022-01-19 22:22:54 +0100

    Fix alert_auditing trigger.

--- mySQL/Tables/telcoservice_db_service_package.sql
+++ mySQL/Tables/telcoservice_db_service_package.sql
@@ -5,6 +5,6 @@ DROP TABLE IF EXISTS `service_package`;
 
 CREATE TABLE `service_package` (
   `ID` int AUTO_INCREMENT PRIMARY KEY,
-  `NAME` varchar(31) CHARACTER SET 'utf8' COLLATE 'utf8_bin',
+  `NAME` varchar(255) ,
   `EXPIRATION_DATE` date DEFAULT NULL  -- NULL = it never expires
 );
--- mySQL/Triggers/trigger_User_strikes.sql
+++ mySQL/Triggers/trigger_User_strikes.sql
@@ -1,20 +1,14 @@
-delimiter // 
+delimiter //
 
 create trigger user_strikes
 after update on telcoservice_db.order
 for each row
 BEGIN
-
-    -- we retrieve the costumer that made the order (distinct is to make the O.CLIENT as a single value)
-	DECLARE costumer varchar(255) default ( SELECT distinct O.CLIENT
-											FROM telcoservice_db.order O
-											WHERE O.client = new.client );
-
     -- if the number of rejection changed (in the order it can only increment) we update the rejections on the client
 	if ( old.number_rejections <> new.number_rejections ) then
 		update telcoservice_db.client as C
 			set C.NUMBER_REJECTIONS = C.NUMBER_REJECTIONS+1
-            where C.username = costumer;
+            where C.username = new.client;
     end if;
 					
 END; //
